c7207ff31bd0016cd46784c04f3e7333cc0efb77
tibirabotto <tibira.botto@gmail.com>
2023-10-24 15:13:21 -0400

    tests add

---
+++ asyncBreeds.js
@@ -0,0 +1,16 @@
+const fs = require('fs');
+
+const breedDetailsFromFile = function(breed, functionToRunWhenThingsAreDone) {
+  console.log('breedDetailsFromFile: Calling readFile...');
+  fs.readFile(`./data/${breed}.txt`, 'utf8', (error, data) => {
+    // CHANGE: Pass data into callback instead of returning it directly
+    console.log("In readFile's Callback: it has the data.");
+    if(error) {
+        functionToRunWhenThingsAreDone(undefined)
+    } else {
+        functionToRunWhenThingsAreDone(data)
+    }
+  });
+};
+
+module.exports = breedDetailsFromFile;
